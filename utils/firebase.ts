8d9d4aa02e6b5de2362d8ec0abfc98d6a9161d15
unallsukru <unallsukru@gmail.com>
2021-08-21 20:47:04 +0300

    last touches to firebase config

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -2,7 +2,8 @@ import firebase from "firebase";
 import { firebaseConfig } from "./firebaseConfig";
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
+if (firebase.apps.length < 1) {
+  firebase.initializeApp(firebaseConfig);
+}
 export const auth = firebase.auth;
 export const firestore = firebase.firestore;
